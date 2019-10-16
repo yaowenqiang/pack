e8f5cf66717a6604c08109d79fc6a02c59bec6c4
yaowenqiang <yaowenqiang111@163.com>
2019-10-17 01:56:31 +0800

    add simple go generate demo

--- math.go
+++ math.go
@@ -1,6 +1,11 @@
 package pack
 
+//go:generate ls -l
+
+//go generate
+
 func Add(i, j int) int {
 	return i + j
 	//return 0
+
 }
--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestCanAddMembers(t *testing.T) {
-	result := Add(1,2)
+	result := Add(1, 2)
 	if result != 3 {
 		t.Log("Failed to add one and two")
 		t.Fail()
