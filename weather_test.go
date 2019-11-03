07fec36a82bf1875721a692940a573d2ac09e141
yaowenqiang <yaowenqiang111@163.com>
2019-11-03 14:25:38 +0800

    add Parallel Benchmark demo

--- weather_test.go
+++ weather_test.go
@@ -5,9 +5,17 @@ import (
 
 
 func BenchmarkPrintWeather(b *testing.B) {
+	/*
 	for i := 0; i < b.N;i++ {
 		PrintWeather(101010100)
 	}
+	*/
+	b.SetParallelism(64)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			PrintWeather(101010100)
+		}
+	})
 
 
 }
