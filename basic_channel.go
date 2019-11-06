6ad885e31c3e6d77ad59a46e834af9391ddbd33f
yaowenqiang <yaowenqiang111@163.com>
2019-11-07 01:55:03 +0800

    add buffered channel demo

--- basic_channel.go
+++ basic_channel.go
@@ -1,11 +1,24 @@
 package main
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
+	phrase := "These are the times that try mans's souls.\n"
+	words := strings.Split(phrase, " ")
+
 	ch := make(chan string, 1)
 	ch <- "Hello"
 	fmt.Println(<-ch)
+
+	ch2 := make(chan string, len(words))
+	for _, word := range words {
+		ch2 <- word
+	}
+
+	for i := 0; i < len(words); i ++ {
+		fmt.Print(<-ch2 + " ")
+	}
 }
 
