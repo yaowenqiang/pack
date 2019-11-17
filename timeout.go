896f1fa58a9211aee860edc66f26451902926011
yaowenqiang <yaowenqiang111@163.com>
2019-11-18 00:53:28 +0800

    remove unused code ,got how  the timer works,

--- timeout.go
+++ timeout.go
@@ -21,16 +21,15 @@ func main() {
 
 	c2 := make(chan string, 1)
 	go func() {
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		c2 <- " result 2"
 	}()
 
 	select {
 	case res := <-c2:
 		fmt.Println(res)
-	case <-time.After(1 * time.Second):
+	case <-time.After(2 * time.Second):
 		fmt.Println("timeout 2")
 	}
 
-	fmt.Scanln()
 }
