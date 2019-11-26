d4e98c7cd6d998540dd73201a29fd67a90421f87
yaowenqiang <yaowenqiang111@163.com>
2019-11-27 02:17:29 +0800

    add more channel demo

--- channel_buffer.go
+++ channel_buffer.go
@@ -3,17 +3,35 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("before chan")
 	message := make(chan string)
-	//message <- "buffered"
+	//message <- "ping"
+	//message <- "pang"
 	//message <- "channel"
-
 	go func() {
-		message <- "ping"
-		message <- "pang"
+		//fmt.Println(<-message)
+		//message <- "buffered"
+		//<-message
+		fmt.Println("put message")
+		fmt.Println(<-message)
 	}()
+	go func() {
+		//fmt.Println(<-message)
+		fmt.Println("get message")
+		message <- "buffered"
+		//<-message
+	}()
+	fmt.Println("after chan")
 
-	fmt.Println(<-message)
-	fmt.Println(<-message)
+	/*
+
+		for i := 0; i < 3; i++ {
+
+		}
+	*/
+
+	//fmt.Println(<-message)
+	//fmt.Println(<-message)
 	//fmt.Println(<-message)
 	//fmt.Scanln()
 }
